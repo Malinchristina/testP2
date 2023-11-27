6b5dfc2288c21367aa3ac383cc9b28fe089972e3
Malinchristina <>
2023-11-27 06:48:25 +0000

    Add qustion file and Q

---
+++ assets/js/questions.js
@@ -0,0 +1,9 @@
+const quizQ = [
+    {
+        question: "Sweden is known for its extensive archipelago. How many islands does the Stockholm archipelago consist of approximately?",
+        option1: "Finish",
+        option2: "Norwegian",
+        option3: "Swedish",
+        answer: 3,
+    },
+]
