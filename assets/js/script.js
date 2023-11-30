0eb8c142186753a77823dac0f2c76e156f737ded
Malinchristina <>
2023-11-30 11:34:51 +0000

    Add to let of buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,42 @@
 // import { quizQuestions } from "./questions.js";
 
 // Load DOM fully before running the quiz
+// Get the button elements and add event listeners
 
 document.addEventListener("DOMContentLoaded", function () {
-    const nameInput = document.getElementsByTagName("name-input");
-    nameInput.focus();
+    const buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function () {
+            if (this.getAttribute("data-type") === "start") {
+                startQuiz();
+            } else if (this.getAttribute("data-type") === "next") {
+                nextQuestion();
+            } else if (this.getAttribute("data-type") === "quit") {
+                quitGame();
+            } else if (this.getAttribute("data-type") === "play-again") {
+                playAgain();
+            }
+        });
+    }
+
 
     // Add button variables
 
-    const playGame = doctype.getElementById("play");
-    const nextGame = doctype.getElementById("next");
-    const quitGame = doctype.getElementById("quit");
-    const playGameAgain = doctype.getElementById("play-again");
-    
+    const playGame = document.getElementById("play");
+    const nextGame = document.getElementById("next");
+    const quitGame = document.getElementById("quit");
+    const playGameAgain = document.getElementById("play-again");
+
+
     // Add event listeners to buttons
 
     playGame.addEventListener("click", startQuiz);
     nextGame.addEventListener("click", nextQuestion);
     quitGame.addEventListener("click", endGame);
     playGameAgain.addEventListener("click", playAgain);
-   // instructions.addEventListener("click", openInstructions);
 
-} )  
+});
 
 // Modal for instructions
 
@@ -30,19 +45,21 @@ const instructionsButton = document.getElementById("instructions");
 let span = document.getElementsByClassName("close")[0];
 instructionsButton.onclick = function () {
     instructionsPopup.style.display = "block";
-}
+};
 // Close modal
 span.onclick = function () {
     instructionsPopup.style.display = "none";
-}
+};
 
 
-// Get the variables
+// Get global variables
 
 const gameArea = document.getElementsByClassName("game-area");
 const questionsArea = document.getElementsByClassName("questions-area");
 const scoreArea = document.getElementsByClassName("score-area");
 const resume = document.getElementsByClassName("resume");
+
+let displayQuestions = document.getElementById("questions");
 
 
 function checkUserName() {
@@ -51,16 +68,22 @@ function checkUserName() {
 }
 
 function startQuiz() {
-    resetScore();
+    const gameArea = document.getElementById("game-area");
+    gameArea.classList.add("hide");
+    questionsArea.classList.add("visible");
 
 }
 
 function showQuestions(questions,) {
+    let;
 
 
 }
 
 function shuffleQuizQuestions() {
+    let randomQuestions = Math.floor(Math.random() * 10);
+    return quizQuestions[randomQuestions];
+
 
 }
 
@@ -73,7 +96,7 @@ function checkAnswer() {
 }
 
 function resetScore() {
-    
+
 }
 
 function playAgain() {
